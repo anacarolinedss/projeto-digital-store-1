ffb91bec505a7b9427862ac8ccbbf2abc1503475
anacarolinedss <anacaroline22santos@gmail.com>
2024-12-17 14:48:23 -0300

    "atualizando gallery"

--- drip-store/src/compoments/Gallery/Gallery.jsx
+++ drip-store/src/compoments/Gallery/Gallery.jsx
@@ -2,11 +2,38 @@
 
 const Gallery = () => {
     return (
+
+        <> 
    
            <div>
-               
+               <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
+  <ol className="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div className="carousel-inner">
+    <div className="carousel-item active">
+      <img className="d-block w-100" src="..." alt="First slide"/>
+    </div>
+    <div className="carousel-item">
+      <img className="d-block w-100" src="..." alt="Second slide"/>
+    </div>
+    <div className="carousel-item">
+      <img className="d-block w-100" src="..." alt="Third slide"/>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
            </div>
-       
+       </>
        )
    
    }
